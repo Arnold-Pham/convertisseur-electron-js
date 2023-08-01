c080fcd5882a6be19561fd51d82e6f3e668a9f68
Arnold Pham <105844490+Arnold-Pham@users.noreply.github.com>
2023-08-01 17:28:48 +0200

    Third commit
    Unsatisfactory try, but here is the commit anyways

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { FileUploader } from "react-drag-drop-files"
+
+const fileTypes = ["MP4", "AVI", "MOV", "OGV", "MPEG"];
 
 export default function App() {
-  return (
-    <div>Hello from React</div>
-  )
+	const [file, setFile] = useState(null);
+
+	const handleChange = (file) => {
+		setFile(file);
+	};
+
+	return (
+		<div className="App">
+			<h1>Hello To Drag & Drop Files</h1>
+			<FileUploader handleChange={handleChange} name="file" types={fileTypes} />
+			<p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+		</div>
+	);
 }
