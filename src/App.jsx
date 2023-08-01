a17a1537a6d531f1fc29c0db40e9bd17d0816ed4
Arnold Pham <105844490+Arnold-Pham@users.noreply.github.com>
2023-08-01 17:59:18 +0200

    Fifth commit
    Ajout d'un peu de CSS et les boutons d'annulation et de conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
-import { FileUploader } from "react-drag-drop-files"
+import { FileUploader } from 'react-drag-drop-files'
+import { generalStyle } from './styles'
 
-const fileTypes = ["MP4", "AVI", "MOV", "OGV", "MPEG"];
+const fileTypes = ['MP4', 'AVI', 'MOV', 'OGV', 'MPEG'];
 
 export default function App() {
 	const [file, setFile] = useState(null);
+	const [history, setHistory] = useState([]);
 
 	const handleChange = (file) => {
 		setFile(file);
 	};
 
 	return (
-		<div className="App">
+		<div className='App'>
 			<h1>Hello To Drag & Drop Files</h1>
-			<FileUploader handleChange={handleChange} name="file" types={fileTypes} />
-			<p>{file ? `File name: ${file.name}` : "no files uploaded yet"}</p>
+			<FileUploader handleChange={handleChange} name='file' types={fileTypes} />
+			<p>{file ? `File name: ${file.name}` : 'no files uploaded yet'}</p>
+			<div className={generalStyle.container}>
+				<button className={generalStyle.button}>Cancel</button>
+				<button className={generalStyle.button}>Convert</button>
+			</div>
 		</div>
 	);
 }
---
+++ src/styles/index.js
@@ -0,0 +1,1 @@
+export { default as generalStyle } from "./general";
