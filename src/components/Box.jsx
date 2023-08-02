41a25e55518d34aceabd794a5eeb0d2a380ceaf0
Arnold Pham <105844490+Arnold-Pham@users.noreply.github.com>
2023-08-02 12:41:57 +0200

    9th commit
    Modification des Box pour implémenter la "progress bar" et modification mineure de style

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,16 +4,19 @@ import { generalStyle } from '../styles';
 export default function Box(props) {
 	return (
 		<div className={generalStyle.box.container}>
-			<div className={generalStyle.box.text}>
-				<p>{props.conv.name}</p>
-				<p>{(props.conv.size / (1024 * 1024)).toFixed(2)} MB</p>
+			<div className={generalStyle.box.top}>
+				<div className={generalStyle.box.text}>
+					<p>{props.conv.name}</p>
+					<p>{(props.conv.size / (1024 * 1024)).toFixed(2)} MB</p>
+				</div>
+				<div>
+					<select className={generalStyle.box.select} name={props.nameFile} id={props.nameFile}>
+						{props.fileTypes.map((value, index) => <option value={index}>{value}</option>)}
+					</select>
+					<button className={generalStyle.button.red}>Delete</button>
+				</div>
 			</div>
-			<div>
-				<select className={generalStyle.box.select} name={props.nameFile} id={props.nameFile}>
-					{props.fileTypes.map((value, index) => <option value={index}>{value}</option>)}
-				</select>
-				<button className={generalStyle.button.red}>Delete</button>
-			</div>
+			<progress className={generalStyle.box.progress}></progress>
 		</div>
 	);
 }
--- src/styles/general.js
+++ src/styles/general.js
@@ -1,14 +1,16 @@
 export default {
-  container: "container px-4 mx-auto",
-  title: "mb-4 pt-5 text-4xl font-extrabold leading-none tracking-tight text-blue-400 md:text-5xl lg:text-6xl text-center",
-  box: {
-    container: "bg-indigo-700 disabled:opacity-75 p-3 my-5 flex justify-between items-center rounded-md",
-    text: "text-white p-3",
-    select: "text-black p-3 me-3 rounded-md"
-  },
-  button: {
-    container: "text-white flex justify-around",
-    red: "bg-red-600 enabled:hover:bg-red-700 enabled:focus:ring-4 enabled:focus:outline-none enabled:focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center enabled:dark:bg-red-500 enabled:dark:hover:bg-red-600 enabled:dark:focus:ring-red-700",
-    blue: "bg-indigo-700 enabled:hover:bg-indigo-800 enabled:focus:ring-4 enabled:focus:outline-none enabled:focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center enabled:dark:bg-indigo-600 enabled:dark:hover:bg-indigo-700 enabled:dark:focus:ring-indigo-800"
-  },
+	container: "container px-4 mx-auto",
+	title: "mb-4 pt-5 text-4xl font-extrabold leading-none tracking-tight text-indigo-500 md:text-5xl lg:text-6xl text-center",
+	box: {
+		container: "bg-indigo-500 p-5 my-5 rounded-md",
+		top: "flex flex-row justify-between items-center mb-3",
+		text: "text-white pe-3",
+		select: "text-black p-2 me-3 rounded-md",
+		progress: "w-full"
+	},
+	button: {
+		container: "text-white flex justify-around",
+		red: "text-white bg-red-600 enabled:hover:bg-red-700 enabled:focus:ring-4 enabled:focus:outline-none enabled:focus:ring-red-300 font-medium rounded-md text-sm px-5 py-2.5 text-center enabled:dark:bg-red-500 enabled:dark:hover:bg-red-600 enabled:dark:focus:ring-red-700",
+		blue: "text-white bg-indigo-600 enabled:hover:bg-indigo-700 enabled:focus:ring-4 enabled:focus:outline-none enabled:focus:ring-indigo-300 font-medium rounded-md text-sm px-5 py-2.5 text-center enabled:dark:bg-indigo-500 enabled:dark:hover:bg-indigo-700 enabled:dark:focus:ring-indigo-700"
+	},
 };
